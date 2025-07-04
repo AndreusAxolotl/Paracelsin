10c9975950fec16bdcd0804048f31d28b20cfa12
Andreus <andreusaxolotl@gmail.com>
2025-07-04 12:38:33 +0200

    importful

--- data.lua
+++ data.lua
@@ -49,6 +49,55 @@ table.insert(data.raw.recipe["fusion-generator"].ingredients, {type = "item", na
 table.insert(data.raw.recipe["fusion-reactor-equipment"].ingredients, {type = "item", name = "electric-coil",       amount = 25})
   end
   
+data.raw.recipe["elevated-pipe"].ingredients = {
+    {type = "item", name = "zinc-pipe", amount = 2},
+    {type = "item", name = "zinc-rivets", amount = 20},
+    {type = "item", name = "iron-stick", amount = 10},
+    {type = "item", name = "zinc-plate", amount = 5},
+  }
+data.raw.recipe["elevated-pipe"].auto_recycle = true
+data.raw.recipe["elevated-pipe"].category = "mechanics"
+data.raw.recipe["elevated-pipe"].subgroup = "zinc-pipe"
+data.raw.item["elevated-pipe"].subgroup = "zinc-pipe"
+data.raw.item["elevated-pipe"].order = "e"
+data.raw.recipe["elevated-pipe"].surface_conditions = {{property = "pressure", min = 5300, max = 5300}}
+data.raw.item["elevated-pipe"].default_import_location = "paracelsin"
+data.raw.item["elevated-pipe"].weight = 50000
+data.raw.recipe["solar-matrix"].energy_required = 20
+data.raw.recipe["solar-matrix"].ingredients = {
+    {type = "item", name = "superconductor", amount = 5},
+    {type = "item", name = "zinc-solder", amount = 10},
+    {type = "item", name = "electric-coil", amount = 2},
+    {type = "item", name = "carbon-fiber", amount = 5},
+    {type = "fluid", name = "nitrogen", amount = 25},
+  }
+data.raw.recipe["solar-matrix"].category = "mechanics"
+data.raw.item["solar-matrix"].order = "d[solar-panel]-b[solar-matrix]"
+data.raw.recipe["solar-matrix"].surface_conditions = {{property = "pressure", min = 5300, max = 5300}}
+data.raw.item["solar-matrix"].default_import_location = "paracelsin"
+data.raw.item["solar-matrix"].weight = 100000
+data.raw.recipe["accumulator-v2"].energy_required = 20
+data.raw.recipe["accumulator-v2"].ingredients = {
+    {type = "item", name = "supercapacitor", amount = 4},
+    {type = "item", name = "zinc-solder", amount = 5},
+    {type = "item", name = "electric-coil", amount = 5},
+    {type = "item", name = "tungsten-carbide", amount = 15},
+    {type = "fluid", name = "nitric-acid", amount = 25},
+  }
+data.raw.recipe["accumulator-v2"].category = "electrochemistry"
+data.raw.recipe["accumulator-v2"].surface_conditions = {{property = "pressure", min = 5300, max = 5300}}
+data.raw.item["accumulator-v2"].default_import_location = "paracelsin"
+data.raw.item["accumulator-v2"].weight = 100000
+
+local function add_player_crafting_categories(categories)
+    local entity = data.raw.character.character
+    for _,category in pairs(categories) do
+      table.insert(entity.crafting_categories, category)
+    end
+  end
+  
+  add_player_crafting_categories({"hand-crafting"})
+
 require "prototypes.items"
 require "prototypes.machines"
 require "prototypes.tiles"
