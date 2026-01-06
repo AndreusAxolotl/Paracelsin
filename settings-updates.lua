20e4b881ef42bb0d98021b2706f0b755ad9a1f37
Andreus <andreusaxolotl@gmail.com>
2026-01-06 17:43:45 +0100

    oh yeah

--- settings-updates.lua
+++ settings-updates.lua
@@ -3,5 +3,5 @@ data.raw["double-setting"]["solar-matrix-power"].allowed_values = {4000}
 data.raw["double-setting"]["solar-matrix-power"].default_value = 4000
 data.raw["double-setting"]["solar-matrix-power"].hidden = true
 data.raw["double-setting"]["accumulator-power-capacity"].allowed_values = {400}
-data.raw["double-setting"]["accumulator-power-capacity"].default_value = 100
+data.raw["double-setting"]["accumulator-power-capacity"].default_value = 400
 data.raw["double-setting"]["accumulator-power-capacity"].hidden = true
