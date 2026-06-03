65c30ba81f8dfa325d7b54c81487326406d005c6
Andreus <andreusaxolotl@gmail.com>
2026-06-03 09:46:20 +0200

    effef

---
+++ data-final-fixes.lua
@@ -0,0 +1,16 @@
+local macerating = require("prototypes.macerating")
+local generate_macerating_recipe = function(item)
+  if item.parameter then return end
+
+  if not data.raw.recipe[item.name .. "-macerating"] then
+      macerating.generate_macerating_recipe(item)
+  end
+end
+
+for type_name in pairs(defines.prototypes.item) do
+  if data.raw[type_name] then
+    for k, item in pairs(data.raw[type_name]) do
+      generate_macerating_recipe(item)
+    end
+  end
+end
